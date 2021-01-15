1aed72cdd66e1d0f03639b598e14e50591ee6bd0
Jose Padron <josepadron.go@gmail.com>
2021-01-15 01:28:48 -0500

    responsive hotfix

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -8,20 +8,22 @@ const viewCode = assets.navbar.viewcode
 
 const NavBar = () => {
   return (
-    <nav>
-      <img className="logo" src={logo.img} alt={logo.alt}/>
-      <ul className="menu">
-        <li><a href="#MyStack">My Stack</a></li>
-        <li><a href="#Portfolio">Portfolio</a></li>
-        <li><a href="#Studies">Studies</a></li>
-        <li><a href="#Articles">Articles</a></li>
-        <li><a href="#Footer">Contact</a></li>
-      </ul>
-      <div className="viewCode">
-        <img src={viewCode.img} alt="view code"/>
-        <a href="https://github.com/padronjosef/Portfolio-React" target="_blank" >View Code</a>
-      </div>
-    </nav>
+    <div className="navbar-container">
+      <nav>
+        <img className="logo" src={logo.img} alt={logo.alt}/>
+        <ul className="menu">
+          <li><a href="#MyStack">My Stack</a></li>
+          <li><a href="#Portfolio">Portfolio</a></li>
+          <li><a href="#Studies">Studies</a></li>
+          <li><a href="#Articles">Articles</a></li>
+          <li><a href="#Footer">Contact</a></li>
+        </ul>
+        <div className="viewCode">
+          <img src={viewCode.img} alt="view code"/>
+          <a href="https://github.com/padronjosef/Portfolio-React" target="_blank" >View Code</a>
+        </div>
+      </nav>
+    </div>
   )
 }
 
