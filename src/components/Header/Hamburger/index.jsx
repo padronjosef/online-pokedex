532314531cc128e843133b8fd43e677626cb8615
Jose Padron <josepadron.go@gmail.com>
2021-02-21 06:19:48 -0500

    fixing CNAME & adding menu missing animation and logic

--- src/components/Header/Hamburger/index.jsx
+++ src/components/Header/Hamburger/index.jsx
@@ -2,21 +2,20 @@ import React from "react";
 import "./style.scss";
 
 const Hamburger = () => {
-  const toggleBurgerBtn = () => {
+  const closeMenu = () => {
     let modal = document.body.style.overflow;
 
     hamburger.classList.toggle("hamburger--is-active");
     header.classList.toggle("header--is-active");
-    
     (modal == 'hidden') ? modal = document.body.style.overflow = 'visible' : modal = document.body.style.overflow = "hidden"
   }
 
   return (
-    <button className="hamburger" id="hamburger" onClick={toggleBurgerBtn}>
+    <div className="hamburger" id="hamburger" onClick={closeMenu}>
       <div className="hamburger__div"></div>
       <div className="hamburger__div"></div>
       <div className="hamburger__div"></div>
-    </button>
+    </div>
   );
 };
 
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,6 +6,13 @@ import './style.scss'
 
 const logo = DB.navbar.logo
 
+const closeMenu = () => {
+  let modal = document.body.style.overflow;
+  header.classList.toggle("header--is-active");
+  hamburger.classList.toggle("hamburger--is-active");
+  (modal == 'hidden') ? modal = document.body.style.overflow = 'visible' : modal = document.body.style.overflow = "hidden"
+}
+
 const Header = () => {
   return (
     <Fragment>
@@ -13,15 +20,15 @@ const Header = () => {
       <header className="header" id="header">
         <nav className="navbar">
           <a href="#" className="logo">
-            <img className="logo__img" src={logo.img} alt={logo.alt}/>
+            <img className="logo__img" onClick={closeMenu} src={logo.img} alt={logo.alt}/>
           </a>
-          <Scrollspy className="menu" items={ ['main', 'stack', 'portfolio', 'studies', 'blog', 'footer'] } currentClassName="menu__item--active" offset={-100}>
+          <Scrollspy className="menu" id="menu" items={ ['main', 'stack', 'portfolio', 'studies', 'blog', 'footer'] } currentClassName="menu__item--active" offset={-100}>
             <li className="menu__item"><a href="#main" className="menu__link"></a></li>
-            <li className="menu__item"><a href="#stack" className="menu__link">Stack</a></li>
-            <li className="menu__item"><a href="#portfolio" className="menu__link">Portfolio</a></li>
-            <li className="menu__item"><a href="#studies" className="menu__link">Studies</a></li>
-            <li className="menu__item"><a href="#blog" className="menu__link">Blog</a></li>
-            <li className="menu__item"><a href="#footer" className="menu__link">Contact</a></li>
+            <li className="menu__item"><a href="#stack" onClick={closeMenu} className="menu__link">Stack</a></li>
+            <li className="menu__item"><a href="#portfolio" onClick={closeMenu} className="menu__link">Portfolio</a></li>
+            <li className="menu__item"><a href="#studies" onClick={closeMenu} className="menu__link">Studies</a></li>
+            <li className="menu__item"><a href="#blog" onClick={closeMenu} className="menu__link">Blog</a></li>
+            <li className="menu__item"><a href="#footer" onClick={closeMenu} className="menu__link">Contact</a></li>
           </Scrollspy>
           <div className="center">
             <a href="https://raw.githubusercontent.com/padronjosef/Portfolio-React/b20042a49ad9a43ea912c1f3e0d42ce2d512112b/src/HV-Jose-Padron.pdf" className="resume">Resume</a>
