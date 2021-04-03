4e9794e99dc80d69834a7ef3f32c6d3b19742691
Jose Padron <josepadron.go@gmail.com>
2021-04-03 01:31:40 -0500

    hotxig btwshow

--- src/components/complements/BtwShow/index.jsx
+++ src/components/complements/BtwShow/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./style.scss";
 
 const BtnShow = ({ onClick, value, total, minToRendered }) => {
-  const text = (total === minToRendered) ? `View all the ${value}`: 'show less' 
+  const text = (total === minToRendered) ? `View all the ${value}`: 'Show Less' 
   
   return (
     <a onClick={onClick} className="btn">
