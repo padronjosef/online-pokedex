56ccdd3e8bd6392b27912f02d09f6f6fc736389b
Jose Padron <josepadron.go@gmail.com>
2021-04-03 01:31:06 -0500

    adding btwshow to studies and fixing styles

--- src/components/Studies/Certificates/index.jsx
+++ src/components/Studies/Certificates/index.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import DB from '/src/DB';
 import "./style.scss";
 
-const Certificates = (props) => (
+const Certificates = ({totalToRender} = props) => (
   <div className="courses" data-aos="zoom-in" data-aos-duration="1500" data-aos-mirror="true">
-    {props.certificates.map(({link, img, name}, index) => (
-      <a className="certificate" target="__blank" href={link} key={index}>
-        <img className="certificate__img" src={img} alt={name} key={index} />
+    {totalToRender.map(({link, img, name, id}) => (
+      <a className="certificate" target="__blank" href={link} key={id}>
+        <img className="certificate__img" src={img} alt={name} />
         <h3 className="certificate__name">{name}</h3>
       </a>
     ))}
