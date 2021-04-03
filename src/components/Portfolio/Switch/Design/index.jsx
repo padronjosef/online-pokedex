7f193c37c1790a67a2f4234842c00176cd665e6f
Jose Padron <josepadron.go@gmail.com>
2021-04-03 02:29:22 -0500

    fixing styles on portfolio component

--- src/components/Portfolio/Switch/Design/index.jsx
+++ src/components/Portfolio/Switch/Design/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import DB from '/src/DB';
 
 const Design = () => (
+  <>
   <div className="projects" data-aos="zoom-in" data-aos-duration="1500" data-aos-mirror="true">
     {DB.designs.slice(0, 9).map(({img,name}, index) => (
       <figure className="projects__figure" key={index} >
@@ -9,6 +10,8 @@ const Design = () => (
       </figure>
     ))}
   </div>
+  <a className="btn" href="https://www.behance.net/gallery/110691601/Platzi-OpenGrafts" target="__black" >View them all</a>
+  </>
 );
 
 export default Design;
