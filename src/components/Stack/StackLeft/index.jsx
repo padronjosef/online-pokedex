3471ebe7983702e53df521af39c96361fbe1b545
Jose Padron <josepadron.go@gmail.com>
2021-04-03 01:32:34 -0500

    stack left hotfix

--- src/components/Stack/StackLeft/index.jsx
+++ src/components/Stack/StackLeft/index.jsx
@@ -1,22 +1,12 @@
 import React from "react";
-import DB from "/src/DB";
+import DB from '/src/DB';
 import "./style.scss";
 
-const StackLeft = ({ handelOpen }) => (
-  <div
-    className="stack-left"
-    data-aos="zoom-in"
-    data-aos-duration="1500"
-    data-aos-mirror="true"
-  >
-    {DB.stack.map(({ img, name } = data, index) => (
-      <figure className="stack-left__figure" key={`data-${index}`}>
-        <img
-          className="stack-left__img"
-          onClick={() => handelOpen(data)}
-          src={img}
-          alt={name}
-        />
+const StackLeft = ({handelOpen}) => (
+  <div className="stack-left" data-aos="zoom-in" data-aos-duration="1500" data-aos-mirror="true">
+    {DB.stack.map((data, index) => (
+      <figure className="stack-left__figure" key={`data-${index}`} >
+        <img className="stack-left__img" onClick={()=> handelOpen(data)} src={data.img} alt={data.name}/>
       </figure>
     ))}
   </div>
