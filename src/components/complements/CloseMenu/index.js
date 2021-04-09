45d0c5fd7d487c1ed19004655ee55961e3c68307
Jose Padron <josepadron.go@gmail.com>
2021-04-08 22:32:47 -0500

    hotfix closeMenu bug

--- src/components/complements/CloseMenu/index.js
+++ src/components/complements/CloseMenu/index.js
@@ -1,8 +1,10 @@
 const closeMenu = () => {
-  let modal = document.body.style.overflow;
-  hamburger.classList.toggle("hamburger--is-active");
-  header.classList.toggle("header--is-active");
-  (modal == 'hidden') ? modal = document.body.style.overflow = 'visible' : modal = document.body.style.overflow = "hidden"
+  if(screen.width < 692) {
+    let modal = document.body.style.overflow;
+    hamburger.classList.toggle("hamburger--is-active");
+    header.classList.toggle("header--is-active");
+    (modal == 'hidden') ? modal = document.body.style.overflow = 'visible' : modal = document.body.style.overflow = "hidden"
+  }
 }
 
 export default closeMenu
