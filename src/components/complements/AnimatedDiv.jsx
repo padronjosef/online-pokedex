ee8bcd3b10a1c189730d8aa7e7b60cedc347dfdd
Jose Padron <josepadron.go@gmail.com>
2022-01-20 21:18:13 -0500

    feature: fixing animatedDiv

--- src/components/complements/AnimatedDiv.jsx
+++ src/components/complements/AnimatedDiv.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
-const AnimatedDiv = ({ id, className, children }) => (
-  <div id={id} className={className} data-aos='zoom-in' data-aos-duration='1500' data-aos-mirror='true'>
-    {children}
-  </div>
-);
+const AnimatedDiv = ({ id, className, children, href }) => {
+  if (href) {
+    return (
+      <a id={id} href={href} className={className} target='_blank' rel='noreferrer'>
+        {children}
+      </a>
+    );
+  }
+  return (
+    <div id={id} className={className} data-aos='zoom-in' data-aos-duration='1500' data-aos-mirror='true'>
+      {children}
+    </div>
+  );
+};
 
 export default AnimatedDiv;
